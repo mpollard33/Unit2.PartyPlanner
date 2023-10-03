5c5c757b8bb15aabd9a9983d6253646d11ab4e87
mpollard33 <mpollard33@gmail.com>
2023-10-03 15:05:14 -0600

    finished party form

---
+++ index.js
@@ -0,0 +1,9 @@
+const API_URL =
+  'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events';
+
+
+  const state = {
+    parties: []
+  }
+
+  const partyList = document.querySelector('#parties');
